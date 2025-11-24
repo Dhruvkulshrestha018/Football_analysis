344e65ba5ddb5f4b421367c8c5108228df4b5a02
Dhruvkulshrestha018 <dhruvkul2004@gmail.com>
2025-11-24 20:38:26 +0530

    Added Ball control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.video_utils import read_video, save_video
 from trackers.tracker import Tracker
 from team_assigner import TeamAssigner
+import numpy as np
 from player_ball_assigner import PlayerBallAssigner
 
 def main():
@@ -22,7 +23,7 @@ def main():
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
     
-    #assign Player Teams
+    
 
     team_assigner = TeamAssigner()
     team_assigner.assign_team_color(video_frames[0], 
@@ -38,15 +39,19 @@ def main():
 
     # Assign Ball Possession
     player_assigner = PlayerBallAssigner()
+    team_ball_control = []
     for frame_num, player_track in enumerate(tracks['players']):
         ball_bbox = tracks['ball'][frame_num][1]['bbox']
         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
         
         if assigned_player != -1:
             tracks['players'][frame_num][assigned_player]['has_ball'] = True
-
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control= np.array(team_ball_control)
     # Draw Output
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
     print(f"[INFO] Output video frames generated: {len(output_video_frames)}")
 
     save_video(output_video_frames, 'output_videos/output_video.avi')
